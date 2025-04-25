2aabd3ec5ff5ac73e01087ac494d5a53003a8994
willakins <willakins23@gmail.com>
2025-04-24 23:27:05 -0400

    Got inception down to 14 mil parameters

--- src/models/Inception/model.py
+++ src/models/Inception/model.py
@@ -5,28 +5,28 @@ class MyInception(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
 
-
         self.inception = models.inception_v3(weights='DEFAULT', aux_logits=True)
+
+        freeze = True
+        for name, param in self.inception.named_parameters():
+            if "fc" in name or "conv2d" in name:  # Only keep the fully connected layers trainable
+                freeze = False
+            param.requires_grad = not freeze
+
         num_features = self.inception.fc.in_features
-        self.inception.fc = nn.Linear(num_features, num_classes)
+        self.inception.fc = nn.Linear(num_features, 512) 
+        self.inception.fc_out = nn.Linear(512, num_classes)
+
         self.dropout = nn.Dropout(0.1)
-        
         self.loss_criterion = nn.CrossEntropyLoss(reduction='mean')
 
     def forward(self, x):
-        if x.shape[1] == 1:  # Grayscale image with 1 channel (maybe unnecessary)
-            x = x.repeat(1, 3, 1, 1)
-        if self.training and self.inception.aux_logits:
-            out, aux_out = self.inception(x)
-            out = self.dropout(out)
-            return out, aux_out
-        else:
-            out = self.inception(x)
-            out = self.dropout(out)
-            return out
-        
+        if x.shape[1] == 1:  # Grayscale image with 1 channel
+            x = x.repeat(1, 3, 1, 1)  # Convert to 3 channels
+        x = self.inception(x)
+        x = self.dropout(x)
+        x = self.inception.fc_out(x)
+        return x
+
     def count_parameters(self):
-        # Count parameters for the main network and auxiliary logits
-        base_params = sum(p.numel() for p in self.inception.parameters() if p.requires_grad)
-        aux_params = sum(p.numel() for p in self.inception.fc.parameters() if p.requires_grad)
-        return base_params + aux_params
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
